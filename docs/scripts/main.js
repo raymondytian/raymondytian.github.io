35624baffaf7d831fe235b301d94b43e93a3c940
r <r@r.local>
2021-08-04 17:12:00 -0700

    Mobile hover

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,14 +1,11 @@
-let coll = document.getElementsByClassName("collapsible");
+let collapsibleLinks = document.querySelectorAll('.collapsible-link');
 
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block") {
-        content.style.display = "none";
-        } 
-        else {
-        content.style.display = "block";
-        }
-    });
-} 
+if (window.matchMedia('(max-width: 767px)').matches) {
+    for (let link of collapsibleLinks) {
+        link.setAttribute("target", "")
+    }
+} else {
+    for (let link of collapsibleLinks) {
+        link.setAttribute("target", "_blank")
+    }
+}
